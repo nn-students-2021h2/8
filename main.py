bea8650daad66ea13eda35077ce6f8368fec875c
RBurtsev <r.a.burtsev@mail.ru>
2021-12-13 04:12:48 +0500

    skelet

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import classBot
+import bot
 
 
 def main():
-    token = ''  # for_token
-    b = classBot.Bot(token)
-    while True:
-        b.work()
+    f = open('token.txt')  # create token.txt wint your token
+    token = f.read()
+    b = bot.Bot(token)
+    b.work()
 
 
 if __name__ == '__main__':
