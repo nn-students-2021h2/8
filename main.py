4dd136bf2c1142232dbc17b5c4408706621f44cd
RBurtsev <50753358+RBurtsev@users.noreply.github.com>
2021-12-14 23:01:30 +0500

    Revert "Rburtsev"

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import bot
+import classBot
 
 
 def main():
-    f = open('token.txt')  # create token.txt wint your token
-    token = f.read()
-    b = bot.Bot(token)
-    b.work()
+    token = ''  # for_token
+    b = classBot.Bot(token)
+    while True:
+        b.work()
 
 
 if __name__ == '__main__':
