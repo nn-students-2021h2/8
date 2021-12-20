5d6c1fe22910691659984c1e8c18a821bf09dc64
RaEzhov <ezhov.mail@yandex.ru>
2021-12-20 12:48:25 +0300

    Image of sqrt graph is creating

--- handling_msg.py
+++ handling_msg.py
@@ -1,5 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy
 
+filename = 'graph.png'
 
 def work(text):
-
-    return text.upper()
+    y = lambda x: np.sqrt(x)
+    fig = plt.subplots()
+    x = np.linspace(0, 100, 20)
+    plt.plot(x, y(x))
+    plt.savefig(filename)
+    return filename
