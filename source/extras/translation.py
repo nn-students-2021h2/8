260bbedf123671c0ada78f58ef06bb2fd1924e4c
Korostast <Korostast@gmail.com>
2022-03-19 01:47:25 +0300

    Hot-fix: incorrect name of translation middleware leads to not working translation system on Linux
    Took 19 minutes

--- source/extras/translation.py
+++ source/extras/translation.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from aiogram.contrib.middlewares.i18n import I18nMiddleware
 
-i18n = I18nMiddleware("Bot", Path(__file__).parents[2] / "locales")
+i18n = I18nMiddleware("bot", Path(__file__).parents[2] / "locales")
 _ = i18n.gettext
 __ = i18n.gettext
 
